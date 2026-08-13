256b3172f99415c45818d67c0a54c60786aeaca9
AiZero12 <Dnhut329@gmail.com>
2026-08-13 19:27:05 +0700

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ===============================
 // Middleware
@@ -1034,10 +1034,6 @@ app.put("/api/users/:user_id", async (req, res) => {
 
 });
 
-app.listen(PORT, () => {
-
-    console.log(
-        `🚀 Server chạy tại http://localhost:${PORT}`
-    );
-
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server đang chạy tại port ${PORT}`);
 });
